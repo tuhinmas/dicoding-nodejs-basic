d83f0331db13bccececcc7d134649f5fc46b1e08
Mastuhin <6450845-tuhinmas@users.noreply.gitlab.com>
2021-06-27 19:52:34 +0700

    routing

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -1,21 +1,40 @@
 const http = require('http');
 
 const requestListener = (request, response) => {
-    response.setHeader('Content-Type','text/html');
+    response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
 
-    const { method } = request;
-    if(method === 'GET'){
-        response.end('<h1>Hello</h1>')
+    const { method, url } = request;
+
+    if (url === '/') {
+        if (method === 'GET') {
+            response.end('<h1>Halaman home!</h1>');
+        }
+        else{
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request!</h1>`);
+        }
     }
-    if(method === 'POST'){
-        response.end('<h1>Hai</h1>')
+    else if (url === '/about') {
+        if (method === 'GET') {
+            response.end('<h1>Halaman about!</h1>');
+        }
+        else if (method === 'POST') {
+            let body = [];
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+
+            request.on('end', () => {
+                const {name} = JSON.parse(body);
+                response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`)
+            });
+        }
+        else{
+            response.end(`<h1>Halaman tidal dapat diakses menggunkana ${method} request</h1>`)
+        }
     }
-    if(method === 'PUT'){
-        response.end('<h1>Bonjour</h1>')
-    }
-    if(method === 'DELETE'){
-        response.end('<h1>Salam</h1>')
+    else{
+        response.end('<h1>Halaman tidak ditemukan!</h1>');
     }
 }
 
